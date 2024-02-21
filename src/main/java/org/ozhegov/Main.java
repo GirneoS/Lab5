7df3b32866aa27f1d5038ea81b31494a0d5bc052
GirS <vntskegr@gmail.com>
2024-02-21 19:32:26 +0300

    .

--- src/main/java/org/ozhegov/Main.java
+++ src/main/java/org/ozhegov/Main.java
@@ -9,15 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        int i = 1;
-//        while(i>0){
-//            i--;
-//            System.out.print(">");
-//            if(scanner.hasNext()){
-//                String[] command = scanner.nextLine().split(" ");
-//                Execution.executeCommand(command);
-//            }
-//        }
+
 
         while(true){
             System.out.print(">");
